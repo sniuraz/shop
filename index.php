17a53c015b59919192ec9e0378f4e7168838a629
sniuraz <sniuraz@gmail.com>
2018-01-02 12:54:45 +0200

    test

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
             //include_once('webpages/main/main-home.php');
             // footer
             //include_once('webpages/footer.php');
+            test
         ?>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
