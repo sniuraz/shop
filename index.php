9e9cb024ec6be7d935a6ccb777806ab1750a1d28
sniuraz <sniuraz@gmail.com>
2018-01-02 12:58:20 +0200

    panaikinom test, pradesim dirbt

--- index.php
+++ index.php
@@ -18,7 +18,6 @@
             //include_once('webpages/main/main-home.php');
             // footer
             //include_once('webpages/footer.php');
-            test
         ?>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
