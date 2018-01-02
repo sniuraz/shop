ce04935576dd96d8fb8339b0665333bfed5b8a71
sniuraz <sniuraz@gmail.com>
2018-01-02 12:39:11 +0200

    test

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Elektroninė parduotuvė - Pagrindinis</title>
+    <!-- <link rel="icon" href="./icons/dimobis.png"> -->
+    <xink rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,600&amp;subset=latin,latin-ext" type="text/css">
+</head>
+    <body>
+    <?php
+            // header
+            // include_once('wp/header.php');
+            // main/home
+            //include_once('webpages/main/main-home.php');
+            // footer
+            //include_once('webpages/footer.php');
+        ?>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
+    </body>
+</html>
+<html>
+</html>
