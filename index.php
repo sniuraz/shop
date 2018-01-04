c810131897f1ea2271b676967524fc3f9aceda28
sniuraz <sniuraz@gmail.com>
2018-01-04 21:17:27 +0200

    vscode

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             // footer
             //include_once('webpages/footer.php');
         ?>
-        vscode
+        vscode 2
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
